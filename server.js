cfba3ddbd2ff7ff5bc8b6784d38fd51e71996772
Pasdesushi <titolivier@gmx.fr>
2021-02-11 18:42:44 +0100

    apply regex filter all request + use sendStatus

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post('/upload/:roomId', upload.single('file'), function (req, res, next) {
       fs.mkdir(uploadPath + req.params.roomId, { recursive: true }, (dirErr) => {
         if (dirErr) {
           console.error(dirErr);
-          res.send(500);
+          res.sendStatus(500);
 
         } else {
           console.log("Directory is created.");
@@ -39,7 +39,7 @@ app.post('/upload/:roomId', upload.single('file'), function (req, res, next) {
             if (err) {
               //throw err
               console.error(err)
-              res.send(500);
+              res.sendStatus(500);
 
             } else {
               console.log("Successfully moved the file!");
@@ -49,7 +49,7 @@ app.post('/upload/:roomId', upload.single('file'), function (req, res, next) {
                 res.send(req.file);
               } catch (httpErr) {
                 console.error(httpErr);
-                res.send(400);
+                res.sendStatus(400);
 
               }
             }
@@ -59,41 +59,52 @@ app.post('/upload/:roomId', upload.single('file'), function (req, res, next) {
 
     } else {
       console.log(req.params.roomId + " not match");
-      res.send(400);
+      res.sendStatus(400);
     }
   } else {
-    res.send(400);
+    res.sendStatus(400);
     //console.log("file null");
   }
 
 });
 
 app.get('/download/:id', function (req, res) {
+  if (req.params.id.match(nameRegex)) {
+    res.download(uploadPath + req.params.id, 'file', function (err) {
+      if (err) {
+        // Handle error, but keep in mind the response may be partially-sent
+        // so check res.headersSent
+        console.log(err)
+        res.sendStatus(404);
 
-  res.download(uploadPath + req.params.id, 'file', function (err) {
-    if (err) {
-      // Handle error, but keep in mind the response may be partially-sent
-      // so check res.headersSent
-      console.log(err)
-      res.send(404);
-    } else {
-      // decrement a download credit, etc.
+      } else {
+        // decrement a download credit, etc.
+      }
     }
+    );
+  } else {
+    console.log(req.params.roomId + " not match");
+    res.sendStatus(400);
   }
-  );
+
 });
 
 app.delete('/delete/:roomId', function (req, res) {
   console.log('request' + req.params.roomId);
-  fs.rm(uploadPath + req.params.roomId, { recursive: true }, function (err) {
-    if (err) {
-      res.sendStatus(404);
-      console.error(err)
-    } else {
-      res.sendStatus(200);
+  if (req.params.roomId.match(nameRegex)) {
+    fs.rm(uploadPath + req.params.roomId, { recursive: true }, function (err) {
+      if (err) {
+        res.sendStatus(404);
+        console.error(err)
+      } else {
+        res.sendStatus(200);
 
-    }
-  });
+      }
+    });
+  } else {
+    console.log(req.params.roomId + " not match");
+    res.sendStatus(400);
+  }
 });
 app.listen(3000, () => {
   console.log('Started on port 3000');
